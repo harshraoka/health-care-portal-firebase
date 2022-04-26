caa6dad64216910ee5c443f7a3b1bc1ff4fcd9aa
harshraoka <81157043+harshraoka@users.noreply.github.com>
2022-04-26 17:01:34 +0530

    Update firebase1.js

--- src/firebase1.js
+++ src/firebase1.js
@@ -2,12 +2,7 @@ import firebase from 'firebase/compat/app';
 import "firebase/firestore";
 
 const config = {
-    apiKey: "AIzaSyD4We15LWmb3gCjoAqhfqota9d54oUDfQg",
-    authDomain: "health-88934.firebaseapp.com",
-    projectId: "health-88934",
-    storageBucket: "health-88934.appspot.com",
-    messagingSenderId: "865231420112",
-    appId: "1:865231420112:web:66fc98efd8732253a9bcd5"
+   //paste your api info here like api keys, auth domain, peoject id, etc.
 };
 
 if (!firebase.apps.length) {
