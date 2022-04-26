c218e45d9f025bcdca4163cad4c1ee47d218165b
harshraoka <81157043+harshraoka@users.noreply.github.com>
2022-04-26 17:01:58 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD4We15LWmb3gCjoAqhfqota9d54oUDfQg",
-  authDomain: "health-88934.firebaseapp.com",
-  projectId: "health-88934",
-  storageBucket: "health-88934.appspot.com",
-  messagingSenderId: "865231420112",
-  appId: "1:865231420112:web:66fc98efd8732253a9bcd5"
+ //paste your api info here like api keys, auth domain, peoject id, etc.
 };
 
 // Use this to initialize the firebase App
